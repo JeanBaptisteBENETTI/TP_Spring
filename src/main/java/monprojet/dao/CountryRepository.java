1f619ba112403dc3a4e9f5c57f007207712f44b8
JeanBaptisteBENETTI <jean-baptiste.benetti@etud.univ-jfc.fr>
2023-01-09 17:23:06 +0100

    Code final.

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -12,10 +12,10 @@ import monprojet.entity.Country;
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-    @Query(value = "SELECT SUM(population)" + " FROM City" + " WHERE id=:idPays", nativeQuery = true)
+    @Query(value = "SELECT SUM(population) AS population" + " FROM City" + " WHERE id=:idPays", nativeQuery = true)
     public Integer getPopulationTotale(Integer idPays);
 
-    @Query(value = "SELECT Country.name, COUNT(City.population)" + " FROM Country INNER JOIN City ON Country.id=City.country_id"
+    @Query(value = "SELECT Country.name AS name, SUM(City.population) AS population" + " FROM Country INNER JOIN City ON Country.id=City.country_id"
         + " GROUP BY Country.name", nativeQuery = true)
     public List<PopulationParPays> populationParPays();
 
